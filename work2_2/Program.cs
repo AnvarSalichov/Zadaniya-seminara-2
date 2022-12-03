d953c2c0983b12bda9fff2d3344d1555d3eeeb1f
Anvar <anv@list.com>
2022-12-03 15:44:14 +0400

    задание2 (показать третью цифру числа)

---
+++ work2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+//  или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число");
+int num=Convert.ToInt32(Console.ReadLine());
+int result= num%10;
+if ((num>99)&&(num<1000))
+    Console.WriteLine(result);
+else if ((num<-99)&&(num>-1000))
+    Console.WriteLine(result);
+else
+ {
+    Console.WriteLine("Третьей цифры в заданном числе нет. Вы ввели неверное число");
+ }   
+
