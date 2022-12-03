cec4e77b761a34f6ae825355430ccd8e98944fab
Anvar <anv@list.com>
2022-12-03 14:58:50 +0400

    задание1 (показать вторую цифру трехзначного числа)

---
+++ work1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int num=Convert.ToInt32(Console.ReadLine());
+int res = (num/10);
+int result= res%10;
+if ((num>99)&&(num<1000))
+    Console.WriteLine(result);
+else
+    Console.WriteLine("Вы ввели неверное число");
+    
